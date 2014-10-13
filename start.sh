322c830b8de28564a1ae45a066774a133d72e9e3
Richard Doktorics <richard.doktorics@sequenceiq.com>
2014-10-13 19:21:56 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -29,4 +29,4 @@ if [ $MISSING_ENV_VARS ]; then
   exit 1;
 fi
 
-cd /uluwatu && unzip /uluwatu/master.zip && cd /uluwatu/uluwatu-master && npm install && node server.js
+cd /uluwatu && unzip /uluwatu/strata.zip && cd /uluwatu/uluwatu-strata && npm install && node server.js
